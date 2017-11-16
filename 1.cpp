6c80767dee63a34bbc6feaa079245c2c58119bb3
Jhonatan Isaí Ramírez Álvarez <31527722+JhonatanIsai05@users.noreply.github.com>
2017-11-16 00:55:21 -0600

    Create  1.cpp

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "info.h"
+
+int main(int argc, char *argv[]){
+	appInfoData("Ejemplo de Punteros", "15/11/2017");
+	
+	int a;
+	a=10;
+	int *puntero;
+	puntero=&a;
+	
+	printf("%d\n", *puntero);
+	
+	return 0;
+}
