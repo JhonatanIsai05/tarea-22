59a10657dbc464b1c7f09cb3453c0c6e87efe9d9
Jhonatan Isaí Ramírez Álvarez <31527722+JhonatanIsai05@users.noreply.github.com>
2017-11-16 01:09:48 -0600

    Create info.h

---
+++ info.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void appInfoData(char *titulo, char *fecha)
+{
+  printf("----------------------------------------------------------------\n");
+  printf("_\n");
+  printf("_       %s\n", titulo);
+  printf("_       Jhonatan Isai Ramirez Alvarez ®\n");
+  printf("_       %s\n", fecha);
+  printf("_\n");
+  printf("----------------------------------------------------------------\n");
+}
